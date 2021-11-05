2b98bf3807cf9b03c982f1052719bb4148e9f061
itzVort3x1 <saikaustubh10@gmail.com>
2021-11-05 08:39:15 +0530

    added tray functionality and positioning

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const electron = require('electron');
 const { app, BrowserWindow, ipcMain, Tray } = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
@@ -10,14 +11,34 @@ app.on('ready', () => {
         width: 300,
         frame: false,
         resizable: false,
+        show: false,
         webPreferences: {
             nodeIntegration: true
         }
     });
-
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
     const iconName = process.platform === 'win32' ? 'clock.png' : 'iconTemplate.png';
     const iconPath = `${__dirname}/src/assets/${iconName}`;
-    new Tray(iconPath);
-})
+    tray = new Tray(iconPath);
+    tray.on('click', (event, bounds) => {
+        // Click event bounds
+        const { x, y } = bounds;
+
+        // Window height and width
+        const { width, height } = mainWindow.getBounds();
+
+        if(mainWindow.isVisible()){
+            mainWindow.hide();
+        }else {
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+            mainWindow.setBounds({
+                x: Math.round(x - width /2),
+                y: Math.round(yPosition),
+                height,
+                width
+            })
+            mainWindow.show();
+        }
+    });
+});
