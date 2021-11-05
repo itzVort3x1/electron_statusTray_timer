495f7321f9265220f52f54d6e4745cec20200739
itzVort3x1 <saikaustubh10@gmail.com>
2021-11-05 13:37:18 +0530

    final code

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 const electron = require('electron');
+const MainWindow = require('./app/main_window');
 const TimerTray = require('./app/timer_tray');
 
-const { app, BrowserWindow, ipcMain, Tray } = electron;
+const { app, ipcMain } = electron;
 
 let mainWindow;
 let tray;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        height: 500,
-        width: 300,
-        frame: false,
-        resizable: false,
-        show: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+    mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
+    // darwin is to check if the operating system is mac
     process.platform === 'darwin' ? app.dock.hide() : mainWindow.setSkipTaskbar(true);
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-    mainWindow.on('blur', () => {
-        mainWindow.hide();
-    });
 
+    // win32 is to check if the operating system is windows
     const iconName = process.platform === 'win32' ? 'alarm.png' : 'iconTemplate.png';
     const iconPath = `${__dirname}/src/assets/${iconName}`;
     tray = new TimerTray(iconPath, mainWindow);
+});
+
+ipcMain.on('update-timer', (event, timeLeft) => {
+    tray.setTitle(timeLeft);
 });
